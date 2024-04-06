9afb8b2f2bfe9113ebacae322e443afeb66a5265
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-04-06 18:19:47 -0500

    remove redundant variable

--- script.lua
+++ script.lua
@@ -19,35 +19,6 @@ alreadyAfk = false
 task = nil
 blockBelowCache = {}
 
--- THANK YOU 5P4(4P5)
-local units = {
-  { base = "second", plural = "seconds", divisor = 1, decimal = 0 },
-  { base = "minute", plural = "minutes", divisor = 60, decimal = 2 },
-  { base = "millisecond", plural = "milliseconds", divisor = 1 / 1000, decimal = 0 },
-  { base = "day", plural = "days", divisor = 60 * 60 * 24, decimal = 5 },
-  { base = "business day", plural = "business days", divisor = (60 * 60 * 24) * (5/7), decimal = 5 },
-  { base = "week", plural = "weeks", divisor = 60 * 60 * 24 * 7, decimal = 7 },
-  { base = "milliday", plural = "millidays", divisor = 60 * 60 * 24 / 1000, decimal = 3 },
-  { base = "beat", plural = "beats", divisor = 60 * 60 * 24 / 1000, decimal = 3 },
-  { base = "year", plural = "years", divisor = 60 * 60 * 24 * 365.25, decimal = 8 },
-  { base = "lunar year", plural = "lunar years", divisor = 60 * 60 * 24 * 365.25 / 29.53059, decimal = 8 },
-  { base = "microfortnight", plural = "microfortnights", divisor = 60 * 60 * 24 * 14 / 1000000, decimal = 1 },
-  { base = "leap year", plural = "leap years", divisor = 60 * 60 * 24 * 365.25 * 4, decimal = 8 },
-  { base = "microcentury", plural = "microcenturies", divisor = 60 * 60 * 24 * 365.25 * 100 / 1000000, decimal = 4 },
-  { base = "nanocentury", plural = "nanocenturies", divisor = 60 * 60 * 24 * 365.25 * 100 / 1000000000, decimal = 1 },
-  { base = "jiffy", plural = "jiffies", divisor = 1 / 60, decimal = 0 },
-  { base = "milliweek", plural = "milliweeks", divisor = 60 * 60 * 24 * 7 / 1000, decimal = 3 },
-  { base = "tick", plural = "ticks", divisor = 1 / 20, decimal = 0 },
-  { base = "ögonblick", plural = "ögonblick", divisor = 2, decimal = 0 },
-  { base = "moment", plural = "moments", divisor = 90, decimal = 3 },
-  { base = "vine", plural = "vines", divisor = 6, decimal = 1 },
-  { base = "Hobbit trilogy", plural = "Hobbit trilogies", divisor = 532 * 60 * 60, decimal = 7 },
-  { base = "Ash Twin cycle", plural = "Ash Twin cycles", divisor = 22 * 60, decimal = 5 },
-  { base = "Quinzième", plural = "Quinzièmes", divisor = (60 * 60 * 24) / 15, decimal = 5 }
-}
-local unit = units[1]
-local current_unit = 1
-
 -- keybinds
 local moveFirstPersonCameraToggle = keybinds:newKeybind("Switch first person camera location", "key.keyboard.backspace", false)
 local ringToggle = keybinds:newKeybind("Toggle health ring", "key.keyboard.right.bracket", false)
