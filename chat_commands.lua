f27337258a0282a8cca551e99bb4fb565e073474
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-04-07 10:18:49 -0500

    Thanks to @superpowers04 on discord for making my coding better

--- chat_commands.lua
+++ chat_commands.lua
@@ -1,51 +1,74 @@
-local function splitString(string, seperator)
+local function splitString(str, seperator)
     if seperator == nil then
         seperator = "%s"
     end
     local tbl = {}
-    for str in string.gmatch(string, "([^" .. seperator .. "]+)") do
-        table.insert(tbl, str)
+    for split in str:gmatch("([^" .. seperator .. "]+)") do
+        table.insert(tbl, split)
     end
     return tbl
+end
+local function printf(tbl)
+    logJson(toJson(tbl))
 end
 
 local prefix = "."
 
 local cmdQueue = {}
-
-local commands = {
+-- Make a local variable for commands so the help command can access it
+local commands
+commands = {
     {
-        -- Since the help command requires access to this table, it is hardcoded. I just have it here so that it shows up
         cmd = "help",
-        desc = "Display commands",
+        desc = "List all commands, their arguments and description",
         args = {},
         func = function(args)
+            local toSend = {
+                { text = "\n---------------", color = "gray" },
+                { text = " COMMANDS ",        color = "green" },
+                { text = "---------------",   color = "gray" },
+            }                                                    -- The commands header
+            for _, v in ipairs(commands) do
+                table.insert(toSend, { text = "\n" .. v.cmd, color = "light_purple" }) -- The command name
+
+                for _, w in ipairs(v.args) do
+                    table.insert(toSend, {
+                        text = w.required and (" {" .. w.arg .. "}") or (" [" .. w.arg .. "]"),
+                        color = w.required and "red" or "yellow",
+                    })                                                                   -- Argument
+                end
+                table.insert(toSend, { text = " | ", color = "gray" })                   -- Seperator
+                table.insert(toSend, { text = v.desc, color = "green" })                 -- Description
+            end
+            table.insert(toSend, { text = "\n----------------------------------------", color = "gray" }) -- Footer
+            printf(toSend)
         end,
     },
     {
         cmd = "ride",
-        desc = "Ride an Entity",
+        desc = "Summon and ride an Entity",
         args = {
             {
-                arg = "str: entity", 
-                required = true
+                arg = "str: entity",
+                required = true,
             },
             {
-                arg = "int: speed", 
-                required = false
-            }
+                arg = "int: speed",
+                required = false,
+            },
         },
         func = function(args)
             local summonStr = ""
 
-            if not args then return false end
+            if not args or #args == 0 then return false end
 
             if #args >= 2 then
-                summonStr = "summon " .. args[1] .. " ~ ~ ~ {Attributes:[{Name:generic.movement_speed,Base:" .. args[2] .. "}],Tags:[\"ToRide\"]}"
-            elseif #args >= 1 then
-                summonStr = "summon " .. args[1] .. " ~ ~ ~ {Tags:[\"ToRide\"]}"
+                summonStr = "summon " ..
+                args[1] ..
+                " ~ ~ ~ {Attributes:[{Name:generic.movement_speed,Base:" ..
+                args[2] .. "}],Tags:[\"ToRide\"]}"
             else
-                return false
+                summonStr = "summon " .. args[1] .. " ~ ~ ~ {Tags:[\"ToRide\"]}"
             end
 
             host:sendChatCommand(summonStr)
@@ -59,94 +82,74 @@ local commands = {
         desc = "Summon a specified amount of an entity",
         args = {
             {
-                arg = "int: amount", 
-                required = true
+                arg = "str: entity",
+                required = true,
             },
             {
-                arg = "str: entity", 
-                required = true
-            }
+                arg = "int: amount",
+                required = false,
+            },
         },
         func = function(args)
-            if not args then return false end
+            if not args or #args < 1 then return false end
+            if args[2] == nil then args[2] = 1 end
 
-            if #args >= 2 then
-                for _ = 1, args[1], 1 do
-                    table.insert(cmdQueue, "summon " .. args[2] .. " ~ ~ ~ {Tags:[\"BulkSummoned\"]}")
-                end
-            else
-                return false
+            for _ = 1, args[2] do
+                table.insert(cmdQueue, "summon " .. args[1] .. " ~ ~ ~ {Tags:[\"BulkSummoned\"]}")
             end
 
             return true
         end,
-    }
+    },
 }
 
-function events.tick()
-    if #cmdQueue >= 1 then
-        if #cmdQueue < 5 then
-            for i = 1, 5, 1 do
-                host:sendChatCommand(cmdQueue[1])
-
-                table.remove(cmdQueue, 1)
-            end
-        else
-            for i = 1, #cmdQueue, 1 do
-                host:sendChatCommand(cmdQueue[1])
-
-                table.remove(cmdQueue, 1)
-            end
-        end
+-- A mirror of the top, using the command name as the index. This makes command access a lot simpler later
+local cmds = {}
+-- Copy everything in commands and set their index to the command name if the index is a number
+for i, cmd in pairs(commands) do
+    if (type(i) == "number") then
+        cmds[cmd.cmd] = cmd
+    else
+        cmds[i] = cmd
     end
 end
 
-function events.CHAT_SEND_MESSAGE(msg)
+events.CHAT_SEND_MESSAGE:register(function(msg)
+    -- No reason to do anything unless the prefix is in the message
+    -- Guard clause instead of massive if statement, this can make code easier to read, understand and shorter
+    if (msg:sub(1, #prefix) ~= prefix) then return msg end
     local split = splitString(msg, " ")
+    -- Instead of looping through the entire table, we remove the prefix from split[1], then index the cmds table
+    local command = cmds[split[1]:sub(#prefix + 1)]
+    -- Return if the command doesn't exist
+    if (not command) then return msg end
 
-    if split[1] == prefix .. "help" then
-        logJson(toJson({text = "\n--------------- COMMANDS ---------------"}))
+    -- Remove the command itself
+    table.remove(split, 1)
+    if command.func(split) == false then
+        local toSend = {
+            { text = "Invalid Command Usage. Usage: ", color = "dark_red" },
+            { text = command.cmd,                      color = "light_purple" },
+        } -- The command name
 
-        for _, v in ipairs(commands) do
-            local toSend = v.cmd
-
-            for _, w in ipairs(v.args) do
-                if w.required then
-                    toSend = toSend .. " {"
-                    toSend = toSend .. w.arg
-                    toSend = toSend .. "}"
-                else
-                    toSend = toSend .. " ["
-                    toSend = toSend .. w.arg
-                    toSend = toSend .. "]"
-                end
-            end
-
-            toSend = toSend .. " - "
-            toSend = toSend .. v.desc
-
-            logJson(toJson({text = "\n" .. toSend}))
+        for _, w in ipairs(command.args) do
+            table.insert(toSend, {
+                text = w.required and (" {" .. w.arg .. "}") or (" [" .. w.arg .. "]"),
+                color = w.required and "red" or "yellow",
+            }) -- Argument
         end
-
-        logJson(toJson({text = "\n----------------------------------------"}))
-
-        host:appendChatHistory(msg)
-
-        return
+        printf(toSend)
     end
+    host:appendChatHistory(msg)
 
-    for _, v in pairs(commands) do
-        if split[1] == prefix .. v.cmd then
-            table.remove(split, 1)
-            if v.func(split) == false then
-                logJson(toJson({text = "Invalid Command Usage. Run " .. prefix .. "help to find the correct usage", color = "red"}))
-            end
+    return
+end, "COMMANDS.SEND_MESSAGE")
 
-            host:appendChatHistory(msg)
-
-            return
-        end
+events.tick:register(function()
+    if (#cmdQueue == 0) then return end -- Return if cmdQueue is empty
+    local count = 5                 -- A count, to not spam commands
+    while #cmdQueue ~= 0 and count > 0 do
+        count = count - 1
+        host:sendChatCommand(table.remove(cmdQueue, 1))
     end
-
-    return msg
-end
+end, "COMMANDS.TICK")
