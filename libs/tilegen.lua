db7726cf3bd10c111f0939941a257ab14855f21d
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-04-07 16:51:37 -0500

    fix tile generation

--- libs/tilegen.lua
+++ libs/tilegen.lua
@@ -5,12 +5,17 @@ function calcTileOrigin(worldOrigin, tileArray, tileSize)
     local y = worldOrigin.y
     local z = worldOrigin.z
 
-    x = x + (tileArray.x * 5) + tileArray.x
+    x = x + (tileArray.x * tileSize.x) + tileArray.x
 
     return vec(x, y, z)
 end
 
 function clone(cornerOne, cornerTwo, destination)
+    log("clone " ..  
+    cornerOne.x .. " " .. cornerOne.y .. " " .. cornerOne.z .. " " .. 
+    cornerTwo.x .. " " .. cornerTwo.y .. " " .. cornerTwo.z .. " " .. 
+    destination.x .. " " .. destination.y .. " " .. destination.z)
+
     host:sendChatCommand("clone " ..  
         cornerOne.x .. " " .. cornerOne.y .. " " .. cornerOne.z .. " " .. 
         cornerTwo.x .. " " .. cornerTwo.y .. " " .. cornerTwo.z .. " " .. 
@@ -20,8 +25,6 @@ end
 
 function putOnGrid(cornerOne, cornerTwo, tileSlot, tileSize, worldOrigin)
     local dest = worldOrigin + vec(-1 * (tileSlot.x * tileSize.x), 1, tileSlot.y * tileSize.y)
-
-    log(cornerOne, cornerTwo, dest)
 
     clone(cornerOne, cornerTwo, dest)
 end
@@ -35,18 +38,17 @@ function funcs:generate(tileArray, tileSize, origin, worldSize)
 
     local tileOrigin = calcTileOrigin(origin, tileArray, tileSize)
 
+    log("Tile Origin: " .. tostring(tileOrigin))
+
     local tiles = {}
 
     for x = 0, tileArray.x-1 do
         for y = 0, tileArray.y-1 do
             local tileX = (tileOrigin.x - (x + (x * tileSize.x)))
-            local tileY = (tileOrigin.y - (y + (y * tileSize.y)))
-            log("Tile at (" .. tostring(tileX) .. ", " .. tostring(tileOrigin.y) .. ", " .. tostring(tileY) .. ")")
+            local tileY = ((tileOrigin.y - (y + ((y * tileSize.y)))) + ((tileSize.y - 5) * 2))
             table.insert(tiles, vec(tileX, tileY))
         end
     end
-
-    log(tileOrigin)
 
     for x = 1, worldSize.x do
         for y = 0, worldSize.y-1 do
@@ -54,7 +56,6 @@ function funcs:generate(tileArray, tileSize, origin, worldSize)
 
             local cornerOne = vec(tile.x, tileOrigin.y, tile.y)
             local cornerTwo = vec(tile.x - tileSize.x, tileOrigin.y + tileSize.z, tile.y - tileSize.y + 1)
-            local cloneTo = vec(origin.x - tileSize.x, origin.y + 1, origin.z)
             putOnGrid(cornerOne, cornerTwo, vec(x, y), tileSize, origin)
         end
     end
