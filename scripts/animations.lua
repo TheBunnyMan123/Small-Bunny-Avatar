e591fee8482c5ed8544bbf35727f27cf138dd243
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-04-19 19:46:14 -0500

    fix entity not loaded in animations.lua

--- scripts/animations.lua
+++ scripts/animations.lua
@@ -10,20 +10,23 @@ function pings.closeChest()
 end
 
 function events.tick()
-    local targetedBlock = player:getTargetedBlock(true, 5).id
-    local target = player:getTargetedEntity(5)
-    local tagetedEntity = ""
-    if target then tagetedEntity = target:getType() end
 
-    if host:isContainerOpen() and not tagetedEntity:find("llama") and not tagetedEntity:find("mule") and not targetedBlock:find("chest") and not targetedBlock:find("hopper") and not targetedBlock:find("barrel") then
-        if not invOpen then
-            pings.openChest()
-            invOpen = true
-        end
-    else
-        if invOpen then
-            pings.closeChest()
-            invOpen = false
+    if host:isHost() and player:isLoaded() then
+        local targetedBlock = player:getTargetedBlock(true, 5).id
+        local target = player:getTargetedEntity(5)
+        local tagetedEntity = ""
+        if target then tagetedEntity = target:getType() end
+        
+        if host:isContainerOpen() and not tagetedEntity:find("llama") and not tagetedEntity:find("mule") and not targetedBlock:find("chest") and not targetedBlock:find("hopper") and not targetedBlock:find("barrel") then
+            if not invOpen then
+                pings.openChest()
+                invOpen = true
+            end
+        else
+            if invOpen then
+                pings.closeChest()
+                invOpen = false
+            end
         end
     end
 end
