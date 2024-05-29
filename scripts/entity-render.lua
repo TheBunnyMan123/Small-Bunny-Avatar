ffd6a5c77a829f6a43214c0c2f34cc6faafb11c4
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-28 21:08:17 -0500

    fixes

--- scripts/entity-render.lua
+++ scripts/entity-render.lua
@@ -7,7 +7,7 @@ if goofy == nil then return end
 ---@param entity Entity.any
 function events.entity_render(delta, entity)
     if entity:getType() == "minecraft:snowball" then
-        if (entity:getPos(delta) - player:getPos(delta)):length() > 2.5 then
+        if (entity:getPos(delta) - client:getCameraPos()):length() > 1 then
             local cube = deepCopy(models.snowball.snowball)
             group:addChild(cube)
 
@@ -18,7 +18,7 @@ function events.entity_render(delta, entity)
         return true
     end
     if entity:getType() == "minecraft:egg" then
-        if (entity:getPos(delta) - player:getPos(delta)):length() > 2.5 then
+        if (entity:getPos(delta) - client:getCameraPos()):length() > 1 then
             local cube = deepCopy(models.egg.egg)
             group:addChild(cube)
 
